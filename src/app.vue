9e6d830b97723f74281f94ca8e91eb0edd626a7e
gaohuan <779312688@qq.com>
2018-01-31 23:13:39 +0800

    add

---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">{{testText}}</div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      testText:"test-vue"
+    }
+  }
+}
+</script>
+<style>
+  #app{
+    color:red
+  }
+</style>
